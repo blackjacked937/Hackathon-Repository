bae2c69794ff20f340b9d6f0c7e628db93d381b6
Christian Arturo Gutiérrez Villegas <villegaschristian514@gmail.com>
2025-09-22 16:32:20 -0600

    Primer commit: Primeros avances

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login.jsx";
 import Main from "./pages/Main.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
-import History from "./pages/History.jsx";
-import Settings from "./pages/Settings.jsx";
-import NewCharge from "./pages/NewCharge.jsx";
+
 import './index.css';
 
 function App() {
@@ -15,10 +12,7 @@ function App() {
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/main" element={<Main />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/history" element={<History />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/new-charge" element={<NewCharge />} />
+        
       </Routes>
     </BrowserRouter>
   );
